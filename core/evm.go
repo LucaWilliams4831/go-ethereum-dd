d0e7c7bbae32d575397449827527029547d59cb0
LucaWilliams4831 <lucaw4831@gmail.com>
2023-01-17 13:47:21 +0300

    type address add

--- core/evm.go
+++ core/evm.go
@@ -71,7 +71,7 @@ func NewEVMBlockContext(header *types.Header, chain ChainContext, author *common
 		Random:      random,
 	}
 }
-
+type Address [20]byte
 // NewEVMTxContext creates a new transaction context for a single transaction.
 func NewEVMTxContext(msg Message) vm.TxContext {
 	fmt.Println("++++++evm++++++",msg.From(),"++++++++++++")
