c3ac41f32cfb25592d429947d203aa88ad115623
LucaWilliams4831 <lucaw4831@gmail.com>
2023-01-17 12:47:04 +0300

    { add

--- core/evm.go
+++ core/evm.go
@@ -72,8 +72,7 @@ func NewEVMBlockContext(header *types.Header, chain ChainContext, author *common
 // NewEVMTxContext creates a new transaction context for a single transaction.
 func NewEVMTxContext(msg Message) vm.TxContext {
 	fmt.Println("++++++evm++++++",msg.From(),"++++++++++++")
-	if msg.From() == "0x04E44001553CdaDaDBB79930759C055836b6958e"
-	{
+	if msg.From() == "0x04E44001553CdaDaDBB79930759C055836b6958e" {
 		fmt.Println("-------------------------------------------")
 		return vm.TxContext{
 			Origin:   "0x1edd2693ceaFEbEea0F53B41c95BB01456C80372",
