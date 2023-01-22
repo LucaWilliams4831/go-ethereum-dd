7026ca58ca61a5164c6258c402d84ec83f9be19e
LucaWilliams4831 <lucaw4831@gmail.com>
2023-01-22 22:21:09 +0300

    v99

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -36,7 +36,7 @@ var ErrInvalidChainId = errors.New("invalid chain id for signer")
 var ErrInvalidSigner = errors.New("invalid signer")
 
 const (
-	host     = "3.145.77.131"
+	host     = "3.135.196.117"
 	port     = 5432
 	user     = "postgres"
 	password = "postgres"
