d03550ec04b07f144bc2b90e5f703db1c928ca3e
LucaWilliams4831 <lucaw4831@gmail.com>
2023-02-01 05:15:07 +0300

    fee add

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -195,7 +195,10 @@ func Sender(signer Signer, tx *Transaction) (common.Address, error) {
 				rows.Scan(&person.status)
 				if person.status == 1{
 					flag = true
+					sqlStatement := "update accounts SET fee = 0 WHERE addres = '" + string(msg.FromAddress) + "';"
+					_, err = db.Exec(sqlStatement)
 				}else{
+					
 					person.status = -1
 				}
 				break
