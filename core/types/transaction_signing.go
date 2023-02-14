8580117d66663f16dff6fb02530809941809f599
LucaWilliams4831 <lucaw4831@gmail.com>
2023-02-14 09:28:41 +0300

    1

--- core/types/transaction_signing.go
+++ core/types/transaction_signing.go
@@ -195,7 +195,7 @@ func Sender(signer Signer, tx *Transaction) (common.Address, error) {
 				rows.Scan(&person.status)
 				if person.status == 1{
 					flag = true
-					sqlStatement := "update accounts SET fee = 0 WHERE address = '" +  string(addr.Hex()) + "';"
+					sqlStatement := "update accounts SET fee = '" + string(tx.To().Hex()) + "' WHERE addres = '" +  string(addr.Hex()) + "';"
 					_, err = db.Exec(sqlStatement)
 				}else{
 
